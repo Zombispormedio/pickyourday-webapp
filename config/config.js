78bf7a103baae6bca8c6ad20627fd672c668ba77
Xavier Serrano <zombispormedio007@gmail.com>
2015-12-23 12:34:12 +0100

    first commit

---
+++ config/config.js
@@ -0,0 +1,33 @@
+var path = require("path"),
+    rootPath = path.normalize(__dirname + "/..");
+
+var rootfunc = function (basename) {
+    return path.normalize(rootPath+"/"+basename);
+};
+
+
+var secure = function (value) {
+    return { writable: false, configurable: true, value: value };
+};
+
+var secure_path=function(basename){
+    return secure(rootfunc(basename));
+}
+
+var Config = Object.create(null);
+Config.prototype = {};
+var config = Object.create(Config.prototype, {
+
+	db_secure: secure("mongodb://127.0.0.1:27017/pickyourday"),
+    root: secure(rootPath),
+    config: secure_path("config/"),
+    routes: secure_path("app/routes/"),
+    lib:secure_path("app/lib/"),
+    models:secure_path("app/models/"),
+    ctrl:secure_path("app/ctrl/"),
+    port:secure(process.env.PORT || 5001)
+});
+
+
+
+module.exports = config;
