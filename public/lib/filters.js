cd505c9c891a576ae374c284e874e1e5c29b8c9d
chespii <andrea.reina12@hotmail.com>
2016-02-10 17:11:31 +0100

    ccc

--- public/lib/filters.js
+++ public/lib/filters.js
@@ -3,3 +3,8 @@ webAppFilter.capitalize=function(){
         return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
     };
 };
+webAppFilter.image=function(ConfigService){
+    return function(input) {
+        return ConfigService.cdn + input;
+    };
+};
---
+++ public/lib/services/config.service.js
@@ -0,0 +1,5 @@
+webAppFactory.ConfigService= function(){
+	return {
+		cdn:"https://8549c43475562a0521ba166a16fdb7de6ce65077.googledrive.com/host/0B-TPTaV5ouD7TUtDXzVxQmhYa1E/"
+	};
+}
