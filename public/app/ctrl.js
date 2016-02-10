58e285b3b88330e4ef4de225fca02b121fbd1c0f
Xavier Serrano <zombispormedio007@gmail.com>
2016-02-10 18:31:32 +0100

    font-awesome

--- public/app/ctrl.js
+++ public/app/ctrl.js
@@ -1,20 +1,4 @@
 webAppController.AppCtrl = function ($rootScope, $scope, CompanyService, $interval) {
 
-    $scope.listPicks=function(){
-        CompanyService.picks().list({}, {}, function(res){
-            $scope.picks=res.data;
-            console.log($scope.picks);
-        }, function(){
-
-
-        });
-    };
-
-
-    $interval(function(){
-        $scope.listPicks();
-
-    }, 3000);
-
- $scope.listPicks();
+    
 };
