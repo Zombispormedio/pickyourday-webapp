0ca8de610ac99098caa03fb0f81b8e1519786d7f
chespii <andrea.reina12@hotmail.com>
2016-02-17 19:53:13 +0100

    cuadro dialogo newService

--- public/app/services/ctrl.js
+++ public/app/services/ctrl.js
@@ -1,4 +1,4 @@
-webAppController.servicesCtrl = function ($scope, CompanyService,  $mdSidenav) {
+webAppController.servicesCtrl = function ($scope, CompanyService,  $mdSidenav, $mdDialog) {
     
     this.getServices=function(){
 		CompanyService.services().get({},function(result){
@@ -15,4 +15,30 @@ webAppController.servicesCtrl = function ($scope, CompanyService,  $mdSidenav) {
 	$scope.resourcesView = function() {
     	$mdSidenav('right').toggle();
   };
+    $scope.showTabDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'app/services/newService.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+        .then(function(answer) {
+          $scope.status = 'You said the information was "' + answer + '".';
+        }, function() {
+          $scope.status = 'You cancelled the dialog.';
+        });
+  };
 };
+
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
