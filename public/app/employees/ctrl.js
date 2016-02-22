5a8725824525b12dba3dec3f3bb54d30d1a91fda
Aaron <auronosmea@hotmail.com>
2016-02-22 16:15:11 +0100

    empleados

---
+++ public/app/employees/ctrl.js
@@ -0,0 +1,12 @@
+webAppController.employeesCtrl = function ($rootScope, $scope, CompanyService) {
+	this.getServices=function(){
+		CompanyService.services().get({},function(result){
+			if(result.error)
+				return console.log(result.error);
+			$scope.services=result.data;
+		}, function(){
+	
+		});
+	}
+	this.getServices();
+};
