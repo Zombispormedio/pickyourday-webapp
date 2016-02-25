ba9a96667d48e020b4e1d3c655697d637825eebe
chespii <andrea.reina12@hotmail.com>
2016-02-25 16:46:03 +0100

    DayDashboard

--- public/app/dayDashboard/ctrl.js
+++ public/app/dayDashboard/ctrl.js
@@ -10,5 +10,22 @@ webAppController.DayDashboardCtrl = function ($scope, CompanyService) {
 		});
 	}
 	this.getEmployees();
-    
+
+    this.getProfile=function(){
+    CompanyService.profile().get({},function(result){
+      if(result.error)
+        return console.log(result.error);
+      $scope.profile=result.data;
+    }, function(){
+
+    });
+  }
+  this.getProfile();
+
+  $scope.getDate=function(){
+  	var f = new Date();
+	var d= f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear();
+	document.getElementById("date").innerHTML=d;
+	window.alert(d);
+  }  	
 };
