535db9d2f391b3b1123506fcec65c7b69b47fd35
chespii <andrea.reina12@hotmail.com>
2016-02-25 17:57:13 +0100

    Vista day dashboard y empleados

--- public/app/employees/ctrl.js
+++ public/app/employees/ctrl.js
@@ -1,4 +1,17 @@
 webAppController.employeesCtrl = function ($rootScope, $scope, CompanyService) {
+	
+	this.getEmployees=function(){
+		CompanyService.employees().get({},function(result){
+			if(result.error)
+				return console.log(result.error);
+			$scope.employees=result.data;
+		}, function(){
+
+		});
+	}
+	this.getEmployees();
+
+
 	this.getServices=function(){
 		CompanyService.services().get({},function(result){
 			if(result.error)
