bfbf6bc6dabf75333dcfa14acd915f8fbbd273dd
Aaron <auronosmea@hotmail.com>
2016-03-06 14:09:01 +0100

    nuevo pick

--- public/app/ownpick/ctrl.js
+++ public/app/ownpick/ctrl.js
@@ -1,3 +1,25 @@
-webAppController.OwnPickCtrl = function (){
+webAppController.OwnPickCtrl = function ($rootScope, $scope, CompanyService){
+	this.getServices=function(){
+		CompanyService.services().get({},function(result){
+			if(result.error)
+				return console.log(result.error);
+			$scope.services=result.data;
+		}, function(){
+
+		});
+
+	}
+	this.getServices();
+
+	this.getEmployees=function(){
+		CompanyService.employees().get({},function(result){
+			if(result.error)
+				return console.log(result.error);
+			$scope.employees=result.data;
+		}, function(){
+	
+		});
+	}
+	this.getEmployees();
     
 }
