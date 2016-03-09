9a664f94ed6e89af38a9e70a2a13e14b7ecf5d8b
Xavier Serrano <zombispormedio007@gmail.com>
2016-03-09 14:08:48 +0100

    -

--- public/lib/services/config.service.js
+++ public/lib/services/config.service.js
@@ -3,6 +3,12 @@ webAppFactory.ConfigService= function(){
         cdn:"https://8549c43475562a0521ba166a16fdb7de6ce65077.googledrive.com/host/0B-TPTaV5ouD7TUtDXzVxQmhYa1E/",
         ServerNotFound: ClosureMessage("Server Not Found"),
 
-        NoRoleAuthorized: ClosureMessage("No Role Authorized")
+        NoRoleAuthorized: ClosureMessage("No Role Authorized"),
+        
+        GoogleMaps:{
+            Path:"https://maps.google.com/maps/api/js",
+            Key:"AIzaSyCmpzeHU3dJ0t-DBIfDwoWC0EZyZAdFVZ0"
+        }
+    
     };
 }
