c32a2a9dd6ed4daf1cccde2759dba14120f05a5e
Xavier Serrano <zombispormedio007@gmail.com>
2016-04-18 18:00:07 +0200

    3d stats

--- public/app/stats/ctrl.js
+++ public/app/stats/ctrl.js
@@ -1,3 +1,21 @@
-webAppController.StatsCtrl = function ($alexandra){
-    console.log($alexandra)
+webAppController.StatsCtrl = function ($scope, $interval){
+  
+    
+    
+     var JustPosition = generate(function() {
+        return { position: RandPosition() };
+    });
+
+
+    function Data_1() {
+        $scope.data_1 = JustPosition();
+    }
+
+    Data_1();
+
+    $interval(Data_1, 1000);
+
+    $scope.config_1 = {
+        streaming: true
+    };
 }
--- public/assets/js/utils.js
+++ public/assets/js/utils.js
@@ -52,3 +52,46 @@ function compareHourString(a, b){
     
     return date_a<date_b;
 }
+
+
+function RandPosition(v) {
+    var range = v || { min: 0, max: 100 };
+    var x = chance.floating(range);
+    var y = chance.floating(range);
+    var z = chance.floating(range);
+    return [x, y, z];
+}
+
+function RandColor(v) {
+    var rangeColor = v || { min: 0, max: 1, fixed: 2 };
+    var r = chance.floating(rangeColor);
+    var g = chance.floating(rangeColor);
+    var b = chance.floating(rangeColor);
+    return [r, g, b, 1];
+}
+
+function RandSize(v) {
+    var range = v || { min: 0.1, max: 2, fixed: 1 };
+    var size = chance.floating(range);
+    return [size, size, size];
+}
+
+
+function RandRotation(v){
+    var range = v || { min: 0, max: 360};
+    var x=Number(chance.bool());
+    var y=Number(chance.bool());
+    var z=Number(chance.bool());
+    return  {
+        angle:chance.integer({ min: 0, max: 360}),
+        axis:[x,y,z]
+    }
+}
+
+
+function generate(fn) {
+    return function() {
+        var count = chance.integer({ min: 0, max: 100 });
+        return Array.apply(0, Array(count)).map(fn);
+    };
+}
