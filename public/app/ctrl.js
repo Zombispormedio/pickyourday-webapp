2a532f773f39466f360bc414f24e24d8f70513cc
Xavier Serrano <zombispormedio007@gmail.com>
2016-04-27 13:00:57 +0200

    logout

--- public/app/ctrl.js
+++ public/app/ctrl.js
@@ -1,4 +1,4 @@
-webAppController.AppCtrl = function ($rootScope,$scope, $mdSidenav,CompanyService) {
+webAppController.AppCtrl = function ($rootScope,$scope, $mdSidenav,CompanyService, OauthService) {
 
   $scope.openLeftMenu = function() {
     $mdSidenav('left').toggle();
@@ -7,9 +7,13 @@ webAppController.AppCtrl = function ($rootScope,$scope, $mdSidenav,CompanyServic
   $scope.closeLeftMenu = function(){
      $mdSidenav('left').close();
   }
-  $scope.logout = function () {        
-        deleteLocal("user");
-        $rootScope.go("login");      
+  $scope.logout = function () {  
+
+	OauthService. logout().Session(function(){
+		 deleteLocal("user");
+        $rootScope.go("login");   
+	}, function(){});
+          
     };
 
    $scope.resourcesView = function() {
