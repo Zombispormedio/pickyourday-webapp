dc16dc47c376787f76d7e75f6b83af28568bdf35
Xavier Serrano <zombispormedio007@gmail.com>
2016-05-09 20:19:17 +0200

    no stats

--- public/app/stats/ctrl.js
+++ public/app/stats/ctrl.js
@@ -1,21 +1,4 @@
 webAppController.StatsCtrl = function ($scope, $interval){
   
-    
-    
-     var JustPosition = generate(function() {
-        return { position: RandPosition() };
-    });
-
-
-    function Data_1() {
-        $scope.data_1 = JustPosition();
-    }
-
-    Data_1();
-
-    $interval(Data_1, 1000);
-
-    $scope.config_1 = {
-        streaming: true
-    };
+   
 }
