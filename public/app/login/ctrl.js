8484f468b236070c629f234bc45ebce9e3c3a77c
Xavier Serrano <zombispormedio007@gmail.com>
2016-05-16 09:14:57 +0200

    message

--- public/app/login/ctrl.js
+++ public/app/login/ctrl.js
@@ -49,7 +49,7 @@ webAppController.LoginCtrl = function ($mdDialog,$rootScope, $scope, OauthServic
             }
         ], function(err, user){
             if(err){
-                return $rootScope.warningToast(err);
+                return $rootScope.warningToast(err.message);
                 
             }
             saveLocal("user", user);
